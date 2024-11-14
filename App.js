f1b8d17b7a9890e9ada158268783544d4280121c
Muneeb912-cmdm <muneebe914@gmail.com>
2024-11-15 00:35:01 +0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import "react-native-gesture-handler";
+
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import { useLoadedAssets } from "./hooks/useLoadedAssets";
+import Navigation from "./navigation";
+import { useColorScheme ,AppRegistry} from "react-native";
+import { PaperProvider } from "react-native-paper";
+import { name as appName } from './app.json';
+
+export default function App() {
+  const isLoadingComplete = useLoadedAssets();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <PaperProvider>
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+      </PaperProvider>
+    );
+  }
+}
+
+AppRegistry.registerComponent(appName, () => Main);
